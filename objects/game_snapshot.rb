e17447217cebc85fcb50d4948cde4c7e751c9d73
Steve Goldman <steve@wespeaktogether.com>
2015-07-28 18:24:31 -0400

    apply election results to snapshot

--- objects/game_snapshot.rb
+++ objects/game_snapshot.rb
@@ -27,21 +27,37 @@ class GameSnapshot
   # utility method for creating a new game
   def GameSnapshot.new_game
     # create a deck of politicians
-    politicians_deck = Politician.from_array_file('data/politicians.json').shuffle
-    # set the initial office holders from the politicians_deck
+    politician_deck = Politician.from_array_file('data/politicians.json').shuffle
+    # set the initial office holders from the politician_deck
     office_holders = [
-      OfficeHolder.new("A", politicians_deck.pop),
-      OfficeHolder.new("B", politicians_deck.pop),
-      OfficeHolder.new("A", politicians_deck.pop),
-      OfficeHolder.new("B", politicians_deck.pop)
+      OfficeHolder.new("A", politician_deck.pop),
+      OfficeHolder.new("B", politician_deck.pop),
+      OfficeHolder.new("A", politician_deck.pop),
+      OfficeHolder.new("B", politician_deck.pop)
     ]
     # create the board
     board = Board.new(StateOfTheUnion.random, office_holders)
-    # create two hands
-    hand_A = Hand.new([ politicians_deck.pop, politicians_deck.pop ])
-    hand_B = Hand.new([ politicians_deck.pop, politicians_deck.pop ])
-    # finally, create the snapshot
-    GameSnapshot.new(board, hand_A, hand_B, politicians_deck)
+    # create the snapshot
+    game_snapshot = GameSnapshot.new(board, Hand.new([]), Hand.new([]), politician_deck)
+    # deal the cards
+    game_snapshot.deal_politicians
+    game_snapshot
+  end
+
+  def apply_election(election)
+    # put the winners in office and losers back in the deck
+    election.candidates_A.each_index do |index|
+      board.office_holders[index] = OfficeHolder.new election.winning_team(index), election.winner(index)
+      politician_deck.push election.loser(index)
+    end
+    # top off the hands
+    deal_politicians
+  end
+  
+  def deal_politicians
+    politician_deck.shuffle
+    (7 - hand_A.politicians.count - board.num_encumbents('A')).times { hand_A.politicians << politician_deck.pop }
+    (7 - hand_B.politicians.count - board.num_encumbents('B')).times { hand_B.politicians << politician_deck.pop }
   end
 
 end

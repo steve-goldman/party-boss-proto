d195bac7900409a30d6ef3714bc857d97e1a894d
Steve Goldman <steve@wespeaktogether.com>
2015-07-30 14:35:08 -0400

    mark decks unordered for equality comparisons

--- objects/game_snapshot.rb
+++ objects/game_snapshot.rb
@@ -10,9 +10,9 @@ class GameSnapshot < BaseObject
     { name: "board",  type: Board },
     { name: "hand_A", type: Hand  },
     { name: "hand_B", type: Hand  },
-    { name: "politician_deck",         type: Politician,      is_array: true },
-    { name: "bill_deck",               type: Bill,            is_array: true },
-    { name: "state_of_the_union_deck", type: StateOfTheUnion, is_array: true },
+    { name: "politician_deck",         type: Politician,      is_array: true, unordered: true },
+    { name: "bill_deck",               type: Bill,            is_array: true, unordered: true },
+    { name: "state_of_the_union_deck", type: StateOfTheUnion, is_array: true, unordered: true },
   ]
 
   # utility method for creating a new game

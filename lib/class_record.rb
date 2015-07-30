e56809d02cb8d3faef6807c9380c3cd67ac36b66
Steve Goldman <steve@wespeaktogether.com>
2015-07-30 01:13:31 -0400

    minor refactor

--- lib/class_record.rb
+++ lib/class_record.rb
@@ -8,10 +8,12 @@ module ClassRecord
   
   def initialize(*members)
     throw "expected arguments: #{self.members.count}" if members.count != self.class::Members.count
+
     @_members = {}
     i = 0
-    self.class::Members.each do |member|
-      @_members[member[:name]] = members[i]
+    self.class::Members.each_index do |index|
+      member = self.class::Members[index]
+      @_members[member[:name]] = members[index]
       # define the accessor for this member (but only once)
       if !self.class.instance_methods(false).include?(member[:name])
         self.class.send(:define_method, member[:name]) do

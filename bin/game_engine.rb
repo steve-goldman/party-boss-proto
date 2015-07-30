8f12421ea72224774d61325833feeb8c068577d8
Steve Goldman <steve@wespeaktogether.com>
2015-07-30 00:03:17 -0400

    clean up game engine impl

--- bin/game_engine.rb
+++ bin/game_engine.rb
@@ -7,37 +7,27 @@ require_relative 'logger'
 
 class GameEngine
 
-  def initialize(game_snapshot, boss_A, boss_B, dice_roller)
-    @game_snapshot = game_snapshot
-    @boss_A = boss_A
-    @boss_B = boss_B
-    @dice_roller = dice_roller
-    @game = Game.new(GameSnapshot.deserialize(@game_snapshot.serialize), [])
-  end
-
-  def GameEngine.new_game
-    game_snapshot = GameSnapshot.new_game
-    GameEngine.new(game_snapshot,
-                   HumanBoss.new("A", game_snapshot.hand_A),
-                   HumanBoss.new("B", game_snapshot.hand_B),
-                   DiceRoller.new)
-  end
-
   def run(num_cycles)
+    game_snapshot = GameSnapshot.new_game
+    boss_A = HumanBoss.new("A", game_snapshot.hand_A)
+    boss_B = HumanBoss.new("B", game_snapshot.hand_B)
+    dice_roller = DiceRoller.new
+    game = Game.new(GameSnapshot.deserialize(game_snapshot.serialize), [])
+    
     num_cycles.times do |index|
       Logger.subheader "Cycle #{index + 1} / #{num_cycles}"
       Logger.header "Election phase"
-      election = run_election
-      @game_snapshot.apply_election election
+      election = Election.run_election game_snapshot, boss_A, boss_B, dice_roller
+      game_snapshot.apply_election election
       Logger.header "Legislative phase"
-      @game.cycles << Cycle.new(election)
-      @game_snapshot.end_cycle
+      game.cycles << Cycle.new(election)
+      game_snapshot.end_cycle
     end
-    @game
+    game
   end
 
   private
 
 end
 
-GameEngine.new_game.run (ARGV.shift || 1).to_i
+GameEngine.new.run (ARGV.shift || 1).to_i

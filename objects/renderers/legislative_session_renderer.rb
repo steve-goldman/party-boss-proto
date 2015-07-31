bf77315a405b1e3c722fafd7ab3b26b38093a1eb
Steve Goldman <steve@wespeaktogether.com>
2015-07-31 12:33:06 -0400

    some cleanup

--- objects/renderers/legislative_session_renderer.rb
+++ objects/renderers/legislative_session_renderer.rb
@@ -6,7 +6,7 @@ class LegislativeSessionRenderer < Renderer
   DoesNotPass = "DOES NOT PASS"
   
   def initialize
-    super(Bill::MaxLength + [Passes.length, DoesNotPass.length].max + 1)
+    super(Bill::MaxLength)
   end
   
   @@instance = nil
@@ -20,7 +20,7 @@ class LegislativeSessionRenderer < Renderer
     [
       "Legislation results",               underline,
       party_header,                        underline,
-      results(legislative_session, board), underline,
+      results(legislative_session, board),
       bills_dealt(legislative_session),    underline
     ].join("\n")
   end
@@ -30,30 +30,31 @@ class LegislativeSessionRenderer < Renderer
   def results(legislative_session, board)
     Config.get.bills_num_on_floor.times.map { |index|
       [
+        two_sides(legislative_session.bills_A[index],
+                  legislative_session.bills_B[index]),
         two_sides(passed(legislative_session, index, 'A'),
                   passed(legislative_session, index, 'B')),
         two_sides(outcomes(legislative_session, index, 'A'),
                   outcomes(legislative_session, index, 'B')),
         two_sides(vps(legislative_session, board, index, 'A'),
-                  vps(legislative_session, board, index, 'B'))
+                  vps(legislative_session, board, index, 'B')),
+        underline,
       ]
-    }.flatten.join("\n")
+    }.join("\n")
   end
 
   def passed(legislative_session, index, party)
-    bill = legislative_session.send("bills_#{party}")[index]
-    passes = legislative_session.passes?(index, party) ? Passes : DoesNotPass
-    "#{bill} #{passes}"
+    legislative_session.passes?(index, party) ? Passes : DoesNotPass
   end
 
   def outcomes(legislative_session, index, party)
     outcomes = legislative_session.send("outcomes_#{party}")[index]
-    "  #{outcomes}"
+    "#{outcomes}"
   end
 
   def vps(legislative_session, board, index, party)
     vps = legislative_session.vps(index, party, board)
-    legislative_session.passes?(index, party) ? "  #{vps} vps" : ""
+    "#{vps} vps"
   end
 
   def bills_dealt(legislative_session)

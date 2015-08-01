4e2114633851c1ab82e9af69e0926aa52fbc4e47
Steve Goldman <steve@wespeaktogether.com>
2015-08-01 17:59:31 -0400

    integer not string

--- src/objects/tactics/params.rb
+++ src/objects/tactics/params.rb
@@ -4,10 +4,10 @@ class Params < BaseObject
 
   # define the data that goes in this object
   Members = [
-    { name: "who",      type: String, can_be_nil: true },
-    { name: "agenda",   type: String, can_be_nil: true },
-    { name: "how_many", type: String, can_be_nil: true },
-    { name: "operator", type: String, can_be_nil: true },
+    { name: "who",      type: String,  can_be_nil: true },
+    { name: "agenda",   type: String,  can_be_nil: true },
+    { name: "how_many", type: Integer, can_be_nil: true },
+    { name: "operator", type: String,  can_be_nil: true },
   ]
 
 end
